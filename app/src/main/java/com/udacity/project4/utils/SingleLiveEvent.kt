5a925c7bd5ca458b0f52e7745bc980d68431c318
matheus-miranda <matheus.sm86@gmail.com>
2021-10-15 16:02:46 -0300

    Save the reminder to the database

--- app/src/main/java/com/udacity/project4/utils/SingleLiveEvent.kt
+++ app/src/main/java/com/udacity/project4/utils/SingleLiveEvent.kt
@@ -16,11 +16,11 @@
 
 package com.udacity.project4.utils
 
-import android.util.Log
 import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import timber.log.Timber
 
 import java.util.concurrent.atomic.AtomicBoolean
 
@@ -44,7 +44,8 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
 
         if (hasActiveObservers()) {
-            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+            Timber.tag(TAG)
+                .w("Multiple observers registered but only one will be notified of changes.")
         }
 
         // Observe the internal MutableLiveData
